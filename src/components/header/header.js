3d82844ee7f7a5f58f33fb58389155055f9544d8
pmos0011 <pawel.moskwa.pm@gmail.com>
2020-09-12 11:54:56 +0200

    LinkedIn, GitHub link added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,25 +5,52 @@ import { connect } from "react-redux";
 import * as actions from '../../store/action';
 import './header-style.css';
 import style from './switch-style';
+import linkedinIcon from '../../images/linkedin.png';
+import githubIcon from '../../images/github.png';
 
 
 const Header = (props) => {
+
     const classes = style();
     const isEnglish = props.content.isEnglish;
+
+    const linkedIn = {
+        backgroundImage: `url(${linkedinIcon})`,
+    }
+
+    const github = {
+        backgroundImage: `url(${githubIcon})`,
+    }
+
+
     return (
-        <div className="switch-container">
-            <Switch
-                classes={
-                    classes
-                }
-                checked={isEnglish}
-                onChange={props.handleChange}
-            />
-            <div className="language-container">
-                <div className="language-label"><label className={ isEnglish ? 'language-label-inactive' : 'language-label-active'}>Polski</label></div>
-                <div className="language-label"><label className={isEnglish ? 'language-label-active' : 'language-label-inactive'}>English</label></div>
+        <>
+            <div className="switch-container">
+                <Switch
+                    classes={
+                        classes
+                    }
+                    checked={isEnglish}
+                    onChange={props.handleChange}
+                />
+                <div className="language-container">
+                    <div className="language-label"><label className={isEnglish ? 'language-label-inactive' : 'language-label-active'}>Polski</label></div>
+                    <div className="language-label"><label className={isEnglish ? 'language-label-active' : 'language-label-inactive'}>English</label></div>
+                </div>
+                <a href="https://www.linkedin.com/in/pawe%C5%82-moskwa-835bab186/"
+                    target="_blank"
+                    rel="noopener"
+                    title="LinkedIn">
+                    <div className="icon-div" style={linkedIn} />
+                </a>
+                <a href="https://github.com/PMos0011"
+                    target="blank"
+                    rel="noopener"
+                    title="GitHub">
+                    <div className="icon-div" style={github} />
+                </a>
             </div>
-        </div>
+        </>
     )
 }
 
